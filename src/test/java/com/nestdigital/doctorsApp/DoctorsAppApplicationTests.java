f1837c5c0c01329f4943169c5b8c6772133a6228
Elda-Dani <eldadani2001@gmail.com>
2022-10-20 01:23:07 +0530

    in

---
+++ src/test/java/com/nestdigital/doctorsApp/DoctorsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nestdigital.doctorsApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
